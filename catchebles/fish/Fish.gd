b5105bbc5a5ae5d33b918087da1acc9e900ebc13
Dmitry Borisov <rundoom1337@gmail.com>
2022-10-02 15:27:48 +0700

    Fish catches only on empty hook

--- catchebles/fish/Fish.gd
+++ catchebles/fish/Fish.gd
@@ -11,6 +11,7 @@ func _ready():
 
 
 func get_hooked(hook_with: RemoteTransform2D):
+	if !hook_with.remote_path.is_empty(): return
 	$Particles2D.emitting = true
 	
 	$CollisionShape2D.set_deferred("disabled", true)
