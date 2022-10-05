d203b24666a07952ca13c2ec0c6b6f59ea5987c2
Dmitry Borisov <rundoom1337@gmail.com>
2022-10-05 20:17:03 +0700

    Destroy animation on Fish

--- catchebles/fish/Fish.gd
+++ catchebles/fish/Fish.gd
@@ -2,13 +2,14 @@ extends KinematicBody2D
 class_name Fish
 
 
-onready var animation_names = $AnimatedSprite.frames.get_animation_names()
+onready var sprite = $AnimatedSprite
+onready var animation_names = sprite.frames.get_animation_names()
 export var velocity = Vector2(-1000, 0)
 
 
 func _ready():
-	$AnimatedSprite.animation = animation_names[randi() % animation_names.size()]
-
+	sprite.animation = animation_names[randi() % animation_names.size()]
+	
 
 func get_hooked(hook_with: RemoteTransform2D):
 	if !get_node_or_null(hook_with.remote_path) == null: return
@@ -26,4 +27,15 @@ func _physics_process(delta):
 
 
 func pass_away():
+	create_particle_texture()
 	queue_free()
+
+
+func create_particle_texture():
+	var frame_res = sprite.frames.get_frame(sprite.animation, sprite.frame).get_data()
+
+	var itex = ImageTexture.new()
+	itex.create_from_image(frame_res, Texture.FLAGS_DEFAULT & ~Texture.FLAG_FILTER)
+	
+	$RemainsParticles.texture = itex
+	$RemainsParticles.emit_then_free()
