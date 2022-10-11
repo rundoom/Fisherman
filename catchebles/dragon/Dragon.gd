fc45c7454f14a44cbf9f92ae75ce002128dfa0bf
Dmitry Borisov <rundoom1337@gmail.com>
2022-10-11 20:01:35 +0700

    Refactor tree

--- catchebles/dragon/Dragon.gd
+++ catchebles/dragon/Dragon.gd
@@ -5,8 +5,8 @@ export var velocity:Vector2
 
 
 func get_hooked(hook_with: RemoteTransform2D) -> bool:
-	if !$Timer.is_stopped(): return false
-	$Timer.start()
+	if !$Catcher/Timer.is_stopped(): return false
+	$Catcher/Timer.start()
 	var hooked_body: Node2D = get_node_or_null(hook_with.remote_path)
 	var catched = get_node_or_null($Catcher.remote_path)
 	if hooked_body is BigFish:
